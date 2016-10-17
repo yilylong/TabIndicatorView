1a9216a2675ecdf39fe7cf5d016ca8c1979389f2
yilylong <yilylong@163.com>
2016-10-17 13:31:42 +0800

    添加上传到jcenter的代码

--- app/src/main/java/com/zhl/tabindicatorviewsample/MainActivity.java
+++ app/src/main/java/com/zhl/tabindicatorviewsample/MainActivity.java
@@ -1,4 +1,4 @@
-package com.zhl.tabindicatorview;
+package com.zhl.tabindicatorviewsample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -19,31 +19,31 @@ public class MainActivity extends AppCompatActivity {
         TabIndicatorView indicatorView = (TabIndicatorView) findViewById(R.id.tab_indicator_view);
         TabIndicatorView indicatorView2 = (TabIndicatorView) findViewById(R.id.tab_indicator_view2);
         ArrayList<TabItem> items = new ArrayList<TabItem>();
-        for(int i=0;i<4;i++){
+        for (int i = 0; i < 4; i++) {
             TabItem item = new TabItem();
-            item.title = "近"+(i+1)+"月";
+            item.title = "近" + (i + 1) + "月";
             item.position = i;
             items.add(item);
         }
         indicatorView.initTabs(items, new TabIndicatorView.OnTabItemCheckListener() {
             @Override
             public void onTabItemCheck(TabItemView itemTabView, int position) {
-                Toast.makeText(MainActivity.this,"当前选中=="+position,Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "当前选中==" + position, Toast.LENGTH_SHORT).show();
             }
         });
         indicatorView.setDefaultCheckedPos(1);
 
         ArrayList<TabItem> items2 = new ArrayList<TabItem>();
-        for(int i=0;i<4;i++){
+        for (int i = 0; i < 4; i++) {
             TabItem item = new TabItem();
-            item.title = "第"+(i+1)+"季度";
+            item.title = "第" + (i + 1) + "季度";
             item.position = i;
             items2.add(item);
         }
         indicatorView2.initTabs(items2, new TabIndicatorView.OnTabItemCheckListener() {
             @Override
             public void onTabItemCheck(TabItemView itemTabView, int position) {
-                Toast.makeText(MainActivity.this,"当前选中=="+position,Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "当前选中==" + position, Toast.LENGTH_SHORT).show();
             }
         });
         indicatorView2.setDefaultCheckedPos(0);
