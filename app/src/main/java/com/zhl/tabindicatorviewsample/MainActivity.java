abca9ba61b40de0d4f10e4793ebdcfdd9798baf7
yilylong <yilylong@163.com>
2017-10-24 11:22:31 +0800

    上传到jitpack

--- app/src/main/java/com/zhl/tabindicatorviewsample/MainActivity.java
+++ app/src/main/java/com/zhl/tabindicatorviewsample/MainActivity.java
@@ -2,15 +2,19 @@ package com.zhl.tabindicatorviewsample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.zhl.tabindicatorview.bean.TabItem;
+import com.zhl.tabindicatorview.view.PswdInputView;
 import com.zhl.tabindicatorview.view.TabIndicatorView;
 import com.zhl.tabindicatorview.view.TabItemView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private int position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TabIndicatorView indicatorView = (TabIndicatorView) findViewById(R.id.tab_indicator_view);
         TabIndicatorView indicatorView2 = (TabIndicatorView) findViewById(R.id.tab_indicator_view2);
+        final PswdInputView inputView = (PswdInputView) findViewById(R.id.tab_indicator_view3);
+        Button remove = (Button) findViewById(R.id.removebtn);
+        Button addbtn = (Button) findViewById(R.id.addBtn);
         ArrayList<TabItem> items = new ArrayList<TabItem>();
         for (int i = 0; i < 4; i++) {
             TabItem item = new TabItem();
@@ -47,5 +54,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         indicatorView2.setDefaultCheckedPos(0);
+        inputView.initTabs(6);
+        remove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(position>0){
+                    --position;
+                }
+                inputView.removePsw(position);
+            }
+        });
+        addbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputView.insertPsw(position);
+                if(position<6){
+                    ++position;
+                }
+            }
+        });
     }
 }
